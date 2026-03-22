68c8e915aa9be0c9a20447735f7f2fd53945bd19
Nicola Scarlino <nicolascarlino@MacBook-Pro-de-Nicola.local>
2026-03-22 15:32:43 +0100

    save all changes

--- src/components/PitchDeck.tsx
+++ src/components/PitchDeck.tsx
@@ -18,7 +18,7 @@ import waterskiPyramid from "./images/waterski_pyramid.jpg";
 import waterskiTeam from "./images/waterski_team.jpg";
 import barefooter from "./images/Barefooter.jpg";
 import childhoodDock from "./images/childhood-dock.jpg";
-import valentinoVid from "./images/secuencia_valentino.mp4";
+import Vid from "./images/secuencia_valentino.mp4";
 import lvVid from "./images/LV_INSTAGRAM_.mp4";
 
 import "../styles/pitch-deck.css";
